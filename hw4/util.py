672714cc7181baee605eb7a75122ba3b06ab5d63
kevin <onedayatatime0923@gmail.com>
2018-05-08 21:15:43 +0800

    vae_complete

--- hw4/util.py
+++ hw4/util.py
@@ -13,7 +13,7 @@ assert time and np
 torch.manual_seed(923)
 
 class DataManager():
-    def __init__(self,latent_dim, discriminator_update_num, generator_update_num):
+    def __init__(self,latent_dim=0, discriminator_update_num=0, generator_update_num=0):
         self.data={}
         self.latent_dim= latent_dim
         self.discriminator_update_num= discriminator_update_num
@@ -105,17 +105,17 @@ class DataManager():
         batch_loss=torch.zeros(2)
         
         data_size= len(self.data[name].dataset)
-        for i, (x, y) in enumerate(self.data[name]):
+        for i, x in enumerate(self.data[name]):
             batch_index=i+1
-            batch_x, batch_y= Variable(x).cuda(), Variable(y).cuda()
+            batch_x= Variable(x).cuda()
             mean_x, logvar_x= encoder(batch_x)
             output= decoder(mean_x, logvar_x,mode='train')
             #output = output.view(-1,output.size()[2])
-            reconstruction_loss = criterion(output,batch_y)
-            kl_divergence_loss= kl_coefficient*torch.sum((-1/2)*( 1+ logvar_x- mean_x**2 - torch.exp(logvar_x)))/(len(x))
+            reconstruction_loss = criterion(output,batch_x)
+            kl_divergence_loss= torch.sum((-1/2)*( 1+ logvar_x- mean_x**2 - torch.exp(logvar_x)))/(len(x))
             encoder_optimizer.zero_grad()
             decoder_optimizer.zero_grad()
-            (reconstruction_loss+ kl_divergence_loss).backward()
+            (reconstruction_loss+ kl_coefficient*kl_divergence_loss).backward()
             encoder_optimizer.step()
             decoder_optimizer.step()
 
@@ -131,7 +131,7 @@ class DataManager():
         print('\nTime: {} | Total  Reconstruction Loss: {:.6f} | Total KL Divergance Loss: {:.6f}  '.format(self.timeSince(start,1),
                     float(total_loss[0])/batch_index, float(total_loss[1])/batch_index))
         print('-'*80)
-    def val_vae(self,name, encoder, decoder, optimizer, epoch, print_every=5, record=0, path=None):
+    def val_vae(self,name, encoder, decoder, optimizer, epoch, print_every=5, sample_n= 50, record=0, path=None):
         start= time.time()
         encoder.eval()
         decoder.eval()
@@ -141,12 +141,12 @@ class DataManager():
         predict=[]
         
         data_size= len(self.data[name].dataset)
-        for i, (x, y) in enumerate(self.data[name]):
+        for i, x in enumerate(self.data[name]):
             batch_index=i+1
-            batch_x, batch_y= Variable(x).cuda(), Variable(y).cuda()
+            batch_x= Variable(x).cuda()
             mean_x, logvar_x= encoder(batch_x)
-            output= decoder(mean_x, logvar_x, mode='val')
-            reconstruction_loss = criterion(output,batch_y)
+            output= decoder(mean_x, logvar_x, mode='test')
+            reconstruction_loss = criterion(output,batch_x)
             kl_divergence_loss= torch.sum((-1/2)*( 1+ logvar_x- mean_x**2 - torch.exp(logvar_x)))/(len(x))
             # loss
             total_loss+= torch.FloatTensor([float(reconstruction_loss), float(kl_divergence_loss)]) # sum up total loss
@@ -160,6 +160,10 @@ class DataManager():
 
         total_loss/=  batch_index
         predict=torch.cat(predict,0)
+
+        sample_x = Variable(torch.normal(torch.zeros(sample_n,self.latent_dim))).cuda()
+        predict= torch.cat((decoder(sample_x,mode='test').cpu().data,predict),0)
+        self.write(predict,path)
         print('\nVal set: Average Reconstruction Loss: {:.6f} | Average KL Divergance Loss: {:.6f} | Time: {}  '.format(float(total_loss[0]),float(total_loss[1]),
                         self.timeSince(start, batch_index*len(batch_x)/ data_size)))
         print('-'*80)
@@ -186,27 +190,30 @@ class DataManager():
         s -= m * 60
         return '%dm %ds' % (m, s)
 class Encoder(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Encoder, self).__init__()
         self.output_size=output_size
         self.conv1 = nn.Sequential(                 # input shape (1, 28, 28)
-            nn.Conv2d(input_size[0], 128, 3, 1, 1),              # output shape (16, 28, 28)
+            nn.Conv2d(input_size[0], hidden_size, 3, 1, 1),              # output shape (16, 28, 28)
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=2),            # output shape (16,  7,  7)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(128, 64, 3, 1, 1),         
+            nn.Conv2d(hidden_size,hidden_size, 3, 1, 1),         
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=2),            
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d( 64, 32, 3, 1, 1),         
+            nn.Conv2d( hidden_size,hidden_size, 3, 1, 1),         
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=2),            
         )
         self.den1= nn.Sequential(
-            nn.Linear( 32*(input_size[1]//8)*(input_size[2]//8),  output_size*2),
-            nn.ReLU(),
+            nn.Linear( hidden_size*(input_size[1]//8)*(input_size[2]//8),  output_size*2),
+            nn.BatchNorm1d(output_size*2),
         )
     def forward(self, x):
         x = self.conv1(x)
@@ -216,54 +223,67 @@ class Encoder(nn.Module):
         x= self.den1(x)
         return x[:,:self.output_size], x[:,self.output_size:]
 class Decoder(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Decoder, self).__init__()
         self.output_size=output_size
-        self.conv1 = nn.Sequential(                 # input shape (1, 28, 28)
-            nn.Conv2d(input_size[0], 128, 3, 1, 1),              # output shape (16, 28, 28)
+        self.den1= nn.Sequential(
+            nn.Linear(input_size, (output_size[1]//8)* (output_size[2]//8) *hidden_size),
+            nn.BatchNorm1d((output_size[1]//8)* (output_size[2]//8) *hidden_size),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            # output shape (16,  7,  7)
+        )
+        self.conv1 = nn.Sequential(                 # input shape (1, 28, 28)
+            nn.ConvTranspose2d(hidden_size,hidden_size, 4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d(hidden_size),
+            nn.ReLU(),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(128, 64, 3, 1, 1),         
+            nn.ConvTranspose2d(hidden_size, hidden_size ,4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d( 64, 32, 3, 1, 1),         
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            
+            nn.ConvTranspose2d(hidden_size, output_size[0],4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d(output_size[0]),
+            nn.Sigmoid(),
         )
-        self.den1= nn.Sequential(
-            nn.Linear( 32*(input_size[1]//8)*(input_size[2]//8),  output_size*2),
-            nn.ReLU(),
-        )
-    def forward(self, x):
+    def forward(self, mean_x, logvar_x=0 , mode='test'):
+        if mode== 'train':
+            epsilon_x= torch.normal(torch.zeros_like(mean_x))
+            x= mean_x + epsilon_x*torch.exp((1/2)*logvar_x)
+        elif mode== 'test':
+            x= mean_x
+        else: raise ValueError('Wrong mode')
+        x= self.den1(x)
+        x = x.view(x.size(0), -1,(self.output_size[1]//8), (self.output_size[2]//8))
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = x.view(x.size(0), -1)
-        x= self.den1(x)
-        return x[:,:self.output_size], x[:,self.output_size:]
+        return x 
 class Generator(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Generator, self).__init__()
+        self.input_size=input_size
+        self.hidden_size=hidden_size
         self.output_size=output_size
         self.den1= nn.Sequential(
-            nn.Linear(input_size, (output_size[1]//8)* (output_size[2]//8) *32),
+            nn.Linear(input_size, (output_size[1]//8)* (output_size[2]//8) *hidden_size),
+            nn.BatchNorm1d((output_size[1]//8)*(output_size[2]//8) *hidden_size),
             nn.ReLU(),
         )
         self.conv1 = nn.Sequential(                 # input shape (1, 28, 28)
-            nn.ConvTranspose2d(32, 64, 4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.ConvTranspose2d(hidden_size, hidden_size, 4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
         )
         self.conv2 = nn.Sequential(
-            nn.ConvTranspose2d(64, 128 ,4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.ConvTranspose2d( hidden_size, hidden_size ,4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d(hidden_size),
             nn.ReLU(),
         )
         self.conv3 = nn.Sequential(
-            nn.ConvTranspose2d(128, output_size[0],4, stride=2, padding=1),# output shape (16, 28, 28)
-            nn.Sigmoid(),
+            nn.ConvTranspose2d( hidden_size, output_size[0],4, stride=2, padding=1),# output shape (16, 28, 28)
+            nn.BatchNorm2d( output_size[0]),
+            nn.Tanh(),
         )
     def forward(self, x):
         x= self.den1(x)
@@ -273,26 +293,29 @@ class Generator(nn.Module):
         x = self.conv3(x)
         return x 
 class Discriminator(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Discriminator, self).__init__()
+        self.input_size=input_size
+        self.hidden_size=hidden_size
         self.output_size=output_size
         self.conv1 = nn.Sequential(                 # input shape (1, 28, 28)
-            nn.Conv2d(input_size[0], 32, 3, 1, 1),              # output shape (16, 28, 28)
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            # output shape (16,  7,  7)
+            nn.Conv2d(input_size[0], hidden_size, 4, 2, 1),              # output shape (16, 28, 28)
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d( 32, 16, 3, 1, 1),         
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            
+            nn.Conv2d( hidden_size, hidden_size, 4, 2, 1),         
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d( 16,  8, 3, 1, 1),         
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2),            
+            nn.Conv2d( hidden_size, hidden_size, 4, 2, 1),         
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
         )
         self.den1= nn.Sequential(
-            nn.Linear(  8*(input_size[1]//8)*(input_size[2]//8),  output_size),
+            nn.Linear(  hidden_size*(input_size[1]//8)*(input_size[2]//8),  output_size),
+            nn.BatchNorm1d(output_size),
             nn.Sigmoid(),
         )
     def forward(self, x):
--- hw4/vae.py
+++ hw4/vae.py
@@ -1,26 +1,30 @@
 
 from util import DataManager, Encoder, Decoder
-assert DataManager and Encoder and Decoder
 import torch
+import torch.nn.functional as F
+assert DataManager and Encoder and Decoder and F
 
 
-BATCH_SIZE= 256
-EPOCHS= 100
+BATCH_SIZE=  64
+EPOCHS= 40
 LATENT_DIM= 512
-KL_DIVERGANCE_COEFFICIENT=0.5
-OUTPUT_DIR= './data/reconstruction'
+HIDDEN_SIZE= 256
+KL_DIVERGANCE_COEFFICIENT= torch.linspace(5E-4,1E-5,EPOCHS).cuda()
+OUTPUT_DIR= './data/vae'
 
-dm = DataManager()
+dm = DataManager(LATENT_DIM)
 train_shape=dm.get_data('train',['./data/train'],BATCH_SIZE, shuffle=True)
 test_shape=dm.get_data('val',['./data/test'],BATCH_SIZE, shuffle=False)
 assert(train_shape== test_shape)
 data_shape=train_shape
 
-encoder= Encoder(data_shape, LATENT_DIM).cuda()
-decoder= Decoder(LATENT_DIM, data_shape).cuda()
+encoder= Encoder(data_shape, HIDDEN_SIZE, LATENT_DIM).cuda()
+decoder= Decoder(LATENT_DIM, HIDDEN_SIZE, data_shape).cuda()
 optimizer= [torch.optim.Adam(encoder.parameters()), torch.optim.Adam(decoder.parameters())]
 
 record=1
 for epoch in range(1,EPOCHS+1):
-    dm.train_vae('train', encoder, decoder, optimizer, epoch, print_every=5)
-    record=dm.val_vae('val', encoder, decoder, optimizer, epoch, print_every=5,record=record, path=OUTPUT_DIR)
+    dm.train_vae('train', encoder, decoder, optimizer, epoch, KL_DIVERGANCE_COEFFICIENT[epoch-1], print_every=5)
+    record=dm.val_vae('val', encoder, decoder, optimizer, epoch, print_every=5,record=0, path=OUTPUT_DIR)
+torch.save(encoder,'encoder.pt')
+torch.save(decoder,'decoder.pt')
