f93c1083313d143ee693afec876b147d46c98849
kevin <onedayatatime0923@gmail.com>
2018-05-29 22:41:30 +0800

    all

--- hw5/train.py
+++ hw5/train.py
@@ -2,7 +2,7 @@
 from util import DataManager, ResNet50_feature
 
 
-EPOCH = 32
+EPOCH =200
 BATCH_SIZE = 2
 HIDDEN_SIZE = 1024
 LABEL_DIM = 11
@@ -12,5 +12,5 @@ dm= DataManager(TENSORBOARD_DIR)
 model= ResNet50_feature(HIDDEN_SIZE, LABEL_DIM).cuda()
 dataloader= dm.get_data('./data/TrimmedVideos/video/train', './data/TrimmedVideos/label/gt_train.csv', save_path=['./data/trainx.npy','./data/trainy.npy'], batch_size= BATCH_SIZE, shuffle= True)
 
-for epoch in range(EPOCH):
+for epoch in range(1,EPOCH+1):
     dm.train( model, dataloader, epoch)
