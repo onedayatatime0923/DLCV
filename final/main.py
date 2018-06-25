818c17b917aec22b4398e3b593755a0108c8b87c
kevin <onedayatatime0923@gmail.com>
2018-06-25 13:40:03 +0800

    all

--- final/main.py
+++ final/main.py
@@ -34,7 +34,7 @@ model= CNN(DROPOUT, PRETRAIN).cuda()
 optimizer = torch.optim.Adam(model.parameters(),lr=LEARNING_RATE)
 
 train_dataloader= DataLoader(EasyDataset(*train_data),batch_size= BATCH_SIZE, shuffle= True, num_workers = 0)
-val_dataloader= DataLoader(EasyDataset(*val_data),batch_size= BATCH_SIZE, shuffle= False, num_workers = 8)
+val_dataloader= DataLoader(EasyDataset(*val_data, flip = False, rotate = False),batch_size= BATCH_SIZE, shuffle= False, num_workers = 8)
 
 accu_record=0
 for epoch in range(1,EPOCH+1):
