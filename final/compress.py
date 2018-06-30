415db6bbc59c49c1d4990d93d5a3154f99a2784a
kevin <onedayatatime0923@gmail.com>
2018-07-01 00:18:08 +0800

    all

--- final/compress.py
+++ final/compress.py
@@ -1,18 +1,9 @@
 
-import os, sys, time, math
+import time, math
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils import data as Data
-import torchvision 
-from skimage import io
-import skimage.transform
 from sklearn.cluster import KMeans
 import pickle
-assert os and np and nn and F and optim and Data and torchvision and io and skimage
-assert pickle and sys
 
 
 class DataManager():
@@ -27,18 +18,17 @@ class DataManager():
         s -= m * 60
         return '%dm %ds' % (m, s)
     def save(self, model, path, cluster= 256):
-        model = model.cpu()
+        model = model.cpu().eval()
         state_dict = model.state_dict()
         weight = {}
         kmeans= KMeans(n_clusters=cluster, random_state=0, n_jobs=4)
 
-        #ResNet-34: 182
-        #VGG-16: 92
-        #50,100,150,182
         for key in state_dict:
             print(key)
             print(state_dict[key].numel())
-            if state_dict[key].numel()< cluster:
+            layer = key.split('.')
+            #if state_dict[key].numel()<= cluster:
+            if layer[0] == 'conv':
                 weight[key] = state_dict[key].numpy()
             else:
                 size = state_dict[key].size()
@@ -56,17 +46,17 @@ class DataManager():
         state_dict = {}
         for key in state_dict:
             if isinstance(state_dict[key], np.ndarray):
-                state_dict[key] = weight[key][0]
+                state_dict[key] = torch.from_numpy(weight[key][0])
             else:
                 quantized_table = state_dict[key][0]
                 quantized_weight = state_dict[key][1]
-                state_dict[key] = quantized_weight[quantized_table]
+                state_dict[key] = torch.from_numpy(quantized_weight[quantized_table])
         model.load_state_dict(state_dict)
 
 
 
 if __name__ == '__main__':
     dm = DataManager()
-    model = torch.load('./model/squeezenet.pt').eval()
+    model = torch.load('./model/squeezenet.pt')
     print(model)
     dm.save(model, './')
